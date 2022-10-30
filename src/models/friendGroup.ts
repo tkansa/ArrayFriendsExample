b6efc00bff07731f6ccc3e5041ebab4b3efecbf2
Tiia Kansa <como2e2@gmail.com>
2022-10-30 11:13:33 -0400

    First commit

---
+++ src/models/friendGroup.ts
@@ -0,0 +1,20 @@
+import { Friend } from "./Friend";
+
+
+// make an array to hold your objects (can be empty at first)
+export let friendGroup: Friend[] = [
+  { name: "Marvin", favoriteSong: "BeepBeepBoop" },
+  { name: "Moe", favoriteSong: "Meoooooooow" },
+  { name: "Yuri", favoriteSong: "doododooodoodoo" },
+];
+
+// make a function to add to your array
+export function addFriend(friend: Friend){
+  friendGroup.push(friend);
+}
+
+// could also add other CRUD functions like delete, edit, etc.
+export function deleteFriend(id: number){
+  // use the id to find the correct friend
+  // and delete it
+}
